7c3e6b832572beec5f16a030427c48df7809a42b
Mateusz Szczap <mszczap@team.mobile.de>
2015-05-03 20:27:33 +0200

    next
    Change-Id: Icbd5db35a90615a3afc3e1463d478532bbfdd51f

--- map.go
+++ map.go
@@ -4,9 +4,13 @@ import "fmt"
 
 func main() {
 	var mapa map[string]int = make(map[string]int)
-	mapa["key"] = 1
+	mapa["key1"] = 1
+	mapa["key2"] = 2
 
-	var val, ok = mapa["key"]
+	for key, value := range mapa {
+		fmt.Println("Key:", key, "Value:", value)
+	}
+	var val, ok = mapa["key1"]
 	if ok {
 		fmt.Println("val found:", val)
 	}
